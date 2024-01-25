7e7c37deba28a02caa7ac0aaf4e10a9eac70249e
Luiz <lfernando488@gmail.com>
2024-01-24 22:56:21 -0300

    CarPostStoreServiceImpl concluído

--- api/src/main/java/com/portal/api/service/CarPostStoreServiceImpl.java
+++ api/src/main/java/com/portal/api/service/CarPostStoreServiceImpl.java
@@ -1,5 +1,6 @@
 package com.portal.api.service;
 
+import com.portal.api.client.CarPostStoreClient;
 import com.portal.api.dto.CarPostDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,19 +11,20 @@ import java.util.List;
 public class CarPostStoreServiceImpl implements CarPostStoreService{
 
     @Autowired
+    private CarPostStoreClient carPostStoreClient;
 
     @Override
     public List<CarPostDTO> getCarForSales() {
-        return null;
+        return carPostStoreClient.carForSaleClient();
     }
 
     @Override
     public void changeCarForSale(CarPostDTO carPostDTO, String id) {
-
+        carPostStoreClient.changeCarForSaleClient(carPostDTO, id);
     }
 
     @Override
     public void removeCarForSale(String id) {
-
+        carPostStoreClient.deleteCarForSaleClient(id);
     }
 }
